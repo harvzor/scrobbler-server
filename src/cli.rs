dc8530f209bfe9cbf7ca96d5703361c6b6a8b948
Harvey <harveyjwilliams@aol.co.uk>
2019-10-27 17:13:00 +0100

    Switched to using dialoguer.

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,11 @@
-use std::io::{stdin,stdout,Write};
+use dialoguer::{theme::ColorfulTheme, Select, Input};
 use crate::drink;
 use crate::drinks;
 
 pub fn cli(drinks: &mut drinks::Drinks) {
-    let action = menu();
+    let user_action = menu_get_action();
 
-    match action {
+    match user_action {
         Some(action_name) => match action_name.as_str() {
             "list_drinks" => println!("{:#?}", drinks),
             "add_drink" => menu_add_drink(drinks),
@@ -16,12 +16,33 @@ pub fn cli(drinks: &mut drinks::Drinks) {
     }
 }
 
-fn menu_add_drink(drinks: &mut drinks::Drinks) {
-    println!("");
-    println!("## Add drink");
-    print!("Drink name: ");
+fn menu_get_action() -> Option<String> {
+    let options = &[
+        "List drinks",
+        "Add drink",
+        "Increment drink",
+    ];
 
-    let user_input = get_user_input();
+    let user_selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
+
+    match user_selection {
+        0 => Some("list_drinks".to_string()),
+        1 => Some("add_drink".to_string()),
+        2 => Some("increment_drink".to_string()),
+        _ => None
+    }
+}
+
+fn menu_add_drink(drinks: &mut drinks::Drinks) {
+    let user_input: String = Input::new()
+        .with_prompt("Drink name")
+        .interact()
+        .unwrap();
 
     let drink = drinks.find(user_input.clone());
 
@@ -37,65 +58,20 @@ fn menu_add_drink(drinks: &mut drinks::Drinks) {
 }
 
 fn menu_increment_drink(drinks: &mut drinks::Drinks) {
-    println!("");
-    println!("## Increment drink");
+    let options: Vec<&String> = drinks.drinks
+        .iter()
+        .map(|x| &x.name)
+        .rev()
+        .collect();
 
-    for (i, drink) in drinks.drinks.iter().enumerate() {
-        println!("{}: {}", i+1, drink.name)
-    }
+    let user_selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select drink")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
 
-    println!("Select a drink: ");
-
-    let user_input = get_user_input();
-
-    let mut user_input_index: usize = user_input.parse().unwrap();
-    user_input_index = user_input_index - 1;
-
-    let drink = drinks.find_by_index(user_input_index);
+    let drink = drinks.find_by_index(user_selection);
 
     drink.increment();
-}
-
-fn menu() -> Option<String> {
-    println!("");
-    println!("## Menu");
-    println!("1: List drinks");
-    println!("2: Add drink");
-    println!("3: Increment drink");
-
-    print!("Your option: ");
-
-    let user_input = get_user_input();
-
-    match user_input.as_str() {
-        "1" => Some("list_drinks".to_string()),
-        "2" => Some("add_drink".to_string()),
-        "3" => Some("increment_drink".to_string()),
-        _ => None
-    }
-}
-
-fn get_user_input() -> String {
-    let mut user_input = String::new();
-
-    let _ = stdout().flush();
-
-    stdin().read_line(&mut user_input)
-        .expect("Did not enter a correct string");
-
-    user_input = clean_user_input(user_input);
-
-    return user_input;
-}
-
-fn clean_user_input(mut user_input: String) -> String {
-    if let Some('\n') = user_input.chars().next_back() {
-        user_input.pop();
-    }
-
-    if let Some('\r') = user_input.chars().next_back() {
-        user_input.pop();
-    }
-
-    return user_input;
 }
